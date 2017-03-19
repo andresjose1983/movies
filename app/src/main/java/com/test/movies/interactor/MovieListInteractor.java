49113bf386b0c9e6ac7108ceb8de7bfe707832b2
andresjose1983 <andresjose1983@gmail.com>
2017-03-19 16:14:16 -0400

    Mejoramiento de la busqueda

--- app/src/main/java/com/test/movies/interactor/MovieListInteractor.java
+++ app/src/main/java/com/test/movies/interactor/MovieListInteractor.java
@@ -4,14 +4,18 @@ import com.test.movies.MovieApp;
 import com.test.movies.R;
 import com.test.movies.contract.MoviesListContract;
 import com.test.movies.model.Genre;
+import com.test.movies.model.Movie;
 import com.test.movies.model.MovieResponse;
 import com.test.movies.util.util.RestClient;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import io.realm.Case;
 import io.realm.Realm;
+import io.realm.RealmList;
+import io.realm.RealmQuery;
 import io.realm.RealmResults;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -87,7 +91,13 @@ public class MovieListInteractor {
                         Realm realm = Realm.getDefaultInstance();
                         realm.beginTransaction();
                         realm.deleteAll();
-                        realm.copyToRealmOrUpdate(movieResponses);
+                        for (MovieResponse movieResponse : movieResponses) {
+                            List<Movie> results = movieResponse.getResults();
+                            for (Movie movie : results) {
+                                movie.setGenre(movieResponse.getGenre());
+                                realm.copyToRealmOrUpdate(movie);
+                            }
+                        }
                         realm.commitTransaction();
                         realm.close();
                         return movieResponses;
@@ -112,12 +122,39 @@ public class MovieListInteractor {
      * Get movies from databases
      */
     public void getMovieResponse() {
-        Realm realm = Realm.getDefaultInstance();
-        realm.where(MovieResponse.class).findAll().asObservable()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<RealmResults<MovieResponse>>() {
+        getMovieResponseByDescription(null);
+    }
+
+    /**
+     * Get movies from databases by description
+     */
+    public void getMovieResponseByDescription(String description) {
+        final Realm realm = Realm.getDefaultInstance();
+
+        Observable.combineLatest(
+                equalTo(where(realm, description), R.string.popular),
+                equalTo(where(realm, description), R.string.top_rated),
+                equalTo(where(realm, description), R.string.upcoming),
+                new Func3<RealmResults<Movie>, RealmResults<Movie>, RealmResults<Movie>,
+                        List<MovieResponse>>() {
                     @Override
-                    public void call(RealmResults<MovieResponse> movieResponses) {
+                    public List<MovieResponse> call(RealmResults<Movie> movies,
+                                                    RealmResults<Movie> movies2,
+                                                    RealmResults<Movie> movies3) {
+                        List<MovieResponse> movieResponses = new ArrayList<>();
+                        if (movies.size() > 0)
+                            movieResponses.add(create(movies));
+                        if (movies2.size() > 0)
+                            movieResponses.add(create(movies2));
+                        if (movies3.size() > 0)
+                            movieResponses.add(create(movies3));
+                        return movieResponses;
+                    }
+                }
+        ).observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<List<MovieResponse>>() {
+                    @Override
+                    public void call(List<MovieResponse> movieResponses) {
                         if (mPresenter != null)
                             mPresenter.onSuccess(movieResponses);
                     }
@@ -125,19 +162,47 @@ public class MovieListInteractor {
     }
 
     /**
-     * Get movies from databases by description
+     * Create new movie response
+     *
+     * @param movies
+     * @return
      */
-    public void getMovieResponseByDescription(String description) {
-        Realm realm = Realm.getDefaultInstance();
-        realm.where(MovieResponse.class)
-                .like("results.title", description, Case.SENSITIVE).findAll().asObservable()
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<RealmResults<MovieResponse>>() {
-                    @Override
-                    public void call(RealmResults<MovieResponse> movieResponses) {
-                        if (mPresenter != null)
-                            mPresenter.onSuccess(movieResponses);
-                    }
-                });
+    private MovieResponse create(final RealmResults<Movie> movies) {
+        MovieResponse movieResponse = new MovieResponse();
+
+        if (movies.size() > 0) {
+            RealmList<Movie> realmList = new RealmList<>();
+            movieResponse.setGenre(movies.get(0).getGenre());
+            realmList.addAll(movies.subList(0, movies.size()));
+            movieResponse.setResults(realmList);
+            return movieResponse;
+        }
+        return null;
+    }
+
+    /**
+     * Where validation white title
+     *
+     * @param realm
+     * @param description
+     * @return RealmQuery<Movie>
+     */
+    private RealmQuery<Movie> where(final Realm realm, String description) {
+        RealmQuery<Movie> where = realm.where(Movie.class);
+
+        if (description != null && !description.isEmpty())
+            where.contains("title", description, Case.INSENSITIVE);
+        return where;
+    }
+
+    /**
+     *
+     * @param where
+     * @param category
+     * @return Observable<RealmResults<Movie>>
+     */
+    private Observable<RealmResults<Movie>> equalTo(RealmQuery<Movie> where, int category) {
+        return where.equalTo("genre.name", MovieApp.getInstance().getString(category))
+                .findAll().asObservable();
     }
 }
